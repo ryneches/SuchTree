ea00b7be593e85ceab2dc450e5ca2eb7b2a2fedb
Russell Neches <russell@vort.org>
2022-06-07 13:52:20 +0900

    pass location of the numpy headers, reasons...

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+import numpy
 
 sourcefiles = [ 'SuchTree/SuchTree.pyx' ]
 
 extensions = [ Extension( 'SuchTree', sourcefiles ) ]
 
-extensions = cythonize( extensions, language_level = "3" )
+extensions = cythonize( extensions, language_level = "3",
+                        include_path = [ numpy.get_include() ] )
 
 setup(
     ext_modules = cythonize( 'SuchTree/SuchTree.pyx' )
