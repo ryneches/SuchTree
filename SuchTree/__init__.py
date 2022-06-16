575e88d7174f33459ad1d3f1f037fa28eeee8840
Russell Neches <russell@vort.org>
2022-06-16 16:45:59 +0900

    correct circular import

--- SuchTree/__init__.py
+++ SuchTree/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#from __future__ import absolute_import
-from .MuchTree import SuchTree, SuchLinkedTrees
+from __future__ import absolute_import
+from MuchTree import SuchTree, SuchLinkedTrees
 
 __version__ = 0.8
