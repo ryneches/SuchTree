7594bc4b9b32e36a54e0d837d3ea0a4ce352d4d0
Russell Neches <russell@vort.org>
2022-06-17 18:30:49 +0900

    1.0 release candidate

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from Cython.Build import cythonize
 import numpy
 
 sourcefiles = [ 'SuchTree/*.pyx' ]
-
 extensions = [ Extension( 'MuchTree', sourcefiles, include_dirs=[numpy.get_include()]) ]
-
 extensions = cythonize( extensions, language_level = "3" )
 
 setup(
