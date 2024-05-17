b671c4c5bf6e257001b2d2ad38023fb0be3d86a8
Russell Neches <russell@vort.org>
2024-05-17 14:33:09 +0900

    update project metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = ( this_directory / 'README.md' ).read_text()
 
 sourcefiles = [ 'SuchTree/*.pyx' ]
 extensions = [ Extension( 'MuchTree', sourcefiles, include_dirs=[numpy.get_include()]) ]
@@ -11,4 +15,6 @@ extensions = cythonize( extensions, compiler_directives={ 'language_level' : '3'
 setup(
     ext_modules = extensions,
     py_modules = [ 'SuchTree' ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
