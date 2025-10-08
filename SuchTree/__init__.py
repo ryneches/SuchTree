23295caed97622a6b3393d7256d2ba5bbaac1aed
Russell Neches <git@vort.org>
2025-10-08 13:55:52 +0900

    generate __version__.py for source distributions

--- SuchTree/__init__.py
+++ SuchTree/__init__.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
 from SuchTree.MuchTree import SuchTree, SuchLinkedTrees
-from .__version__ import __version__
+#from .__version__ import __version__
+
+# Import version - will be available from __version__.py after build
+try:
+    from SuchTree.__version__ import __version__
+except ImportError:
+    # Fallback for development when __version__.py doesn't exist yet
+    try:
+        from importlib.metadata import version, PackageNotFoundError
+        try:
+            __version__ = version("SuchTree")
+        except PackageNotFoundError:
+            __version__ = "0.0.0+dev"
+    except ImportError:
+        __version__ = "0.0.0+dev"
 
 # Import exceptions last to avoid circular dependencies
 from .exceptions import (
